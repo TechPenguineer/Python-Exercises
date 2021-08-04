c2e9db62842acf152c30b0a7fe1b43010745b715
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:37:53 -0400

    ex 6

---
+++ Exercise-6/answer.py
@@ -0,0 +1,6 @@
+### The answer is:
+
+# HELLO, {NAME}!
+
+# The reason it makes it all capitalized is because we passed a function into the string that takes each
+# character and converts it into all uppercase. 
---
+++ Exercise-6/question.py
@@ -0,0 +1,5 @@
+# What will the output of this be?
+
+name = input("What is your name? \n")
+upperCaseName = name.upper();
+print(f"HELLO, {upperCaseName}!")
