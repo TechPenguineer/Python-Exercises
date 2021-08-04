85d859fc2175248d421651319409e050e5323de0
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:28:19 -0400

    ex 4

---
+++ Exercise-4/answer.py
@@ -0,0 +1,5 @@
+### The answer is:
+
+# 15
+
+# This is because the computer reads that variable value instead of the name. Its not trying to add the 2 names but the 2 values.
---
+++ Exercise-4/question.py
@@ -0,0 +1,7 @@
+# What will the output of this be?
+
+term1 = 5
+term2 = 10
+
+sum = term1+term2
+print(sum)
