412d453eb5e6b033f7087f4433f6b3443dcf9cab
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:07:55 -0400

    Ex. 1

---
+++ Exercise-1/problem.py
@@ -0,0 +1,5 @@
+# Why wont this work?
+
+int bikes = "I like to ride bikes!"
+print(f"Hello my name is Suzzie and {bikes}")
+
---
+++ Exercise-1/solution.py
@@ -0,0 +1,6 @@
+### The reason that will not work is because you are trying to assign a int type variable to a string.
+## The correct way of doing that would be to do this:
+
+bikes = "I like to ride bikes!"
+print(f"Hello my name is Suzzie and {bikes}")
+
