23723d869077013c4fa73a8e0210a8c086b4de31
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:31:42 -0400

    Ex 5

---
+++ Exercise-5/answer.py
@@ -0,0 +1,7 @@
+### The answer is:
+
+# 26
+
+# The way this works is it makes a function that takes to arguments aka the two terms. It then stores the math equation 
+# ValueOne+ValueTwo in a variable called sum. When we return the sum using 'return sum' that makes it so you can store
+# the functions output in a variable. The value that will be stored in the variable is the sum.
---
+++ Exercise-5/question.py
@@ -0,0 +1,9 @@
+# What will the output of this be?
+
+def Add(ValueOne, ValueTwo):
+    sum = ValueOne+ValueTwo
+    return sum
+
+mathQuestion = Add(11,15)
+
+print(mathQuestion)
