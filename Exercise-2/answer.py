af8b2961cebc3768b74d3e8dcf226f6d67a2749d
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:13:46 -0400

    ex 2

---
+++ Exercise-2/answer.py
@@ -0,0 +1,6 @@
+# If you guessed the output to be:
+
+# My favourite foods are apples and bananas
+
+# You would be correct!
+# This is because when calling items from a list you always start from the number 0 to call the first item 
---
+++ Exercise-2/question.py
@@ -0,0 +1,6 @@
+# What will the output of this be?
+
+
+favouriteFood = ["apples","bananas"]
+
+print("My favourite food are " + favouriteFood[0] + " and " + favouriteFood[1])
