3575b06a56dd3063183f0255de4cb2cc5bc5722c
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 14:22:32 -0400

    ex 3

---
+++ Exercise-3/answer.py
@@ -0,0 +1,16 @@
+### A common mistake with this is people will thing its 11 but its not.
+### Its actualy 10. This is because in programming numbers dont start
+### at 1. They start at 0. Therefore the program runs the code with 
+### the following result:
+
+# ----> 0
+#       1
+#       2
+#       3
+#       4
+#       5
+#       6
+#       7
+#       8
+#       9
+#      10
---
+++ Exercise-3/question.py
@@ -0,0 +1,5 @@
+# What will the output of this be?
+
+sushi=11
+for frogs in range(sushi):
+    print(frogs)
