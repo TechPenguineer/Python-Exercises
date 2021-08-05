75cafeb2500c33a03f70b5c15d30d8bf8d5c59db
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 09:37:53 -0400

    ex 9

---
+++ Exercise-9/answer.py
@@ -0,0 +1,6 @@
+### The answer is:
+
+sentence="Apples are very good for you!"
+
+sentence_words=sentence.split()
+print(sentence_words)
---
+++ Exercise-9/question.py
@@ -0,0 +1,3 @@
+# Parse this string into an array of words
+
+sentence="Apples are very good for you!"
